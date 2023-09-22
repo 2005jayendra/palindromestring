7c4d67a8726bc7a77efca2bb6d76f4e61692b243
2005jayendra <145786539+2005jayendra@users.noreply.github.com>
2023-09-22 21:08:39 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class HelloWorld {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String str=””;
+int count=0;
+for(int i=0;i<s.length();i++){
+char c=s.charAt(i);
+if(c == 'A' || c == 'E' || c == 'I' || c == '0' || c == 'U' || c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+continue;
+}
+str+=c;
+}
+System.out.print(str);
+}
+}
